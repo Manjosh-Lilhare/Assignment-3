f314591c1adea1ee25aa0a5d3bbd10e0f9c36859
Manjosh Lilhare <lilharemanjosh@gmail.com>
2025-10-26 02:00:48 +0530

    Assignment 3 done

---
+++ script.js
@@ -0,0 +1,44 @@
+const red = document.getElementById('Red');
+const blue = document.getElementById('Blue');
+const green = document.getElementById('Green');
+const yellow = document.getElementById('Yellow');
+//text = text.value;
+//const h1=document.getElementById('greet')
+//h1.innerText="Hello "+text;
+
+//function redclick() {
+//    console.log("red clicked");
+//    red.style.backgroundColor = "red";
+//}
+//function blueclick() {
+//    blue.style.backgroundColor = "blue";
+//    console.log("blue clicked");
+//}
+//function greenclick() {
+//    green.style.backgroundColor = "green";
+//    console.log("green clicked");
+//}
+//function yellowclick() {
+//    yellow.style.backgroundColor = "yellow";
+//    console.log("yellow clicked");
+//}
+function greetUser() {
+  const name = document.getElementById('name').value;
+  document.getElementById('greet').innerText = "Hello " + name + "!";
+}
+red.addEventListener('click', ()=>
+    {
+    red.style.backgroundColor = "red";
+});
+blue.addEventListener('click', ()=>
+    {
+    blue.style.backgroundColor = "blue";
+});
+green.addEventListener('click', ()=>
+    {
+    green.style.backgroundColor = "green";
+});
+yellow.addEventListener('click', ()=>
+    {
+    yellow.style.backgroundColor = "yellow";
+});
